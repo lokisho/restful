e2104284db981ab2020b8a3a7ffdf270e1fd7cff
Oscar Sanchez <oscar.sanchez@gorillalogic.com>
2016-08-25 23:15:32 -0600

    Step 1: Create Class

---
+++ sites/all/modules/custom/atlas/src/Plugin/resource/v1_0/Labels__1_0.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lokisho
+ * Date: 8/25/16
+ * Time: 10:02 PM
+ */
+
+namespace Drupal\atlas\Plugin\resource\v1_0\labels__1_0;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+
+class Labels__1_0 implements ResourceInterface {
+
+}
