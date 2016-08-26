e7e20065226a64d4ae842c14bd69a6579072b351
Oscar Sanchez <oscar.sanchez@gorillalogic.com>
2016-08-25 23:28:36 -0600

    Step 2: [BUGFIX] namespace

--- sites/all/modules/custom/atlas/src/Plugin/resource/v1_0/Labels__1_0.php
+++ sites/all/modules/custom/atlas/src/Plugin/resource/v1_0/Labels__1_0.php
@@ -6,7 +6,7 @@
  * Time: 10:02 PM
  */
 
-namespace Drupal\atlas\Plugin\resource\v1_0\labels__1_0;
+namespace Drupal\atlas\Plugin\resource\v1_0;
 use Drupal\restful\Plugin\resource\ResourceNode;
 
 /**
