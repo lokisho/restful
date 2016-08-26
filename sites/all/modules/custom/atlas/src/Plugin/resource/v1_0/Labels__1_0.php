4481aeea0dddefede4918772c133fed2feebbbac
Oscar Sanchez <oscar.sanchez@gorillalogic.com>
2016-08-25 23:18:20 -0600

    Step 2: Create Resource

--- sites/all/modules/custom/atlas/src/Plugin/resource/v1_0/Labels__1_0.php
+++ sites/all/modules/custom/atlas/src/Plugin/resource/v1_0/Labels__1_0.php
@@ -7,8 +7,30 @@
  */
 
 namespace Drupal\atlas\Plugin\resource\v1_0\labels__1_0;
-use Drupal\restful\Plugin\resource\ResourceInterface;
+use Drupal\restful\Plugin\resource\ResourceNode;
 
-class Labels__1_0 implements ResourceInterface {
+/**
+ * Class Labels__1_0
+ * @package Drupal\atlas\Plugin\resource
+ *
+ * @Resource(
+ *   name = "labels:1.0",
+ *   resource = "labels",
+ *   label = "labels",
+ *   description = "Export the Record Labels content type.",
+ *   authenticationTypes = TRUE,
+ *   authenticationOptional = TRUE,
+ *   dataProvider = {
+ *     "entityType": "node",
+ *     "bundles": {
+ *       "record_label"
+ *     }
+ *   },
+ *   formatter = "json_api",
+ *   majorVersion = 1,
+ *   minorVersion = 0
+ * )
+ */
+class Labels__1_0 extends ResourceNode {
 
 }
